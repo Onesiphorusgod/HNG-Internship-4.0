a159dcd7b9e1910b125e243240c2e8f3d5444508
Ayo Apantaku <onesiphorusgod@gmail.com>
2018-04-10 01:03:13 +0100

    HNG Internship stage 2 my php file

---
+++ index.php
@@ -0,0 +1,37 @@
+
+
+<html>
+    <head>
+    <title>HNG Internship Website</title>
+    <link href="style.css" rel="stylesheet" type="text/css"/>
+    <!-- See the change I mage up there-->
+    </head>
+    <body>
+      <header>
+
+            <div class="row">
+            <ul class="main-nav">
+              <li class="active"><a href=""> HOME </a></li>
+              <li><a href=""> ABOUT US </a></li>
+              <li><a href=""> CONTACT </a></li>
+              <li><a href=""> FAQ </a></li>
+              <li><a href=""> NEWS </a></li>
+            </ul>
+
+            </div>
+
+            <div class="Title">
+              <h1> WELCOME GUEST </h1>
+                <h2> ...home away from home </h2>
+                  <h3> HNG Internship Stage 1 Task</h3>
+                    <h4> Time Check: 6:18pm</h4>
+                      <h5> Date: 08 April 2018</h5>
+
+            </div>
+            <div class="clockcontainer">
+              <p><?php echo date("H:i:sa")?></p>
+            </div>
+      </header>
+
+    </body>
+</html>
